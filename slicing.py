a34e2def668302555f9b7f2c2f427b462e959cff
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-18 18:23:29 +0100

    Add level 1 slicing task

---
+++ slicing.py
@@ -0,0 +1,45 @@
+string_ex = "abcdefghij"
+
+# Task 1
+# Extract "cde"
+
+# print(string_ex[2:5])
+# print(string_ex[-8:-5])
+
+# Task 2
+# Extract "abc"
+
+# print(string_ex[:3])
+# print(string_ex[0:3])
+# print(string_ex[-10:-7])
+
+# Task 3
+# Extract everything except first and last char → "bcdefghi"
+
+# print(string_ex[1:-1])
+# print(string_ex[-9:-1])
+
+# Task 4
+# Extract every second character → "acegi"
+
+# print(string_ex[::2])
+# print(string_ex[0:-1:2])
+
+# Task 5
+# Write a slice that returns "jihgfedcba" (reversed)
+
+# print(string_ex[::-1])
+# print(string_ex[-1:1:-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
