96bf2b2b428b78aa8b40b7c853514f9c91a575f7
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 17:16:16 +0100

    Slicing practice

---
+++ slicing_practice.py
@@ -0,0 +1,42 @@
+string_ex = "abcdefghij"
+
+# Level - Basics
+# Task 1
+# Extract "cde"
+print(string_ex[2:5])
+print(string_ex[-8:-5])
+
+# Task 2
+# Extract "abc"
+print(string_ex[:3])
+print(string_ex[0:3])
+print(string_ex[-10:-7])
+
+# Task 3
+# Extract everything except first and last char → "bcdefghi"
+print(string_ex[1:-1])
+print(string_ex[-9:-1])
+
+# Task 4
+# Extract every second character → "acegi"
+print(string_ex[::2])
+print(string_ex[0:-1:2])
+
+# Task 5
+# Write a slice that returns "jihgfedcba" (reversed)
+print(string_ex[::-1])
+print(string_ex[-1:1:-1])
+
+# Level 2 — Negative indexes
+# Task 6 - Get last 3 chars using negative slicing
+print(string_ex[-3:])
+
+# Task 7 - Get "fgh" using only negative indexes
+print(string_ex[-5:-2])
+
+# Task 8 - Reverse the string except the first and last char
+print(string_ex[-2:-10:-1])
+
+# Task 9 - Get every second char but reversed
+print(string_ex[::2][::-1])
+print(string_ex[::-1][::2])
