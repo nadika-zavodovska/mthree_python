d4038a73e70dc2a0e50c1c7d0050acb05d63f6e8
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 17:12:28 +0100

    Static method

---
+++ static_method.py
@@ -0,0 +1,11 @@
+class Calculator:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def sub(x, y):
+        return x - y
+
+print(Calculator.add(10, 20))
+print(Calculator.sub(10, 5))
