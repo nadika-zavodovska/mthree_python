ecbe167048e74f774773061c06d28d2419f94e9f
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 12:59:21 +0100

    Add Activity 2 in python syntax

--- activities_assessments/activities_python_syntax.py
+++ activities_assessments/activities_python_syntax.py
@@ -3,5 +3,16 @@
 # Print("Python is fun!")
 
 # Fixed code
-# we use lowercase for functions. Python is case-sensitive language. 
+# We use lowercase for functions. Python is case-sensitive language.
 print("Python is fun!")
+
+# ***
+
+# Activity 2
+# Display the text in quotation marks to an output block 
+# without moving any of the existing acode to a different line
+# print("Python is fun!") print("Python is also easy.")
+
+# Fixed code
+# Add ; to make possible write multiple lines on one line
+print("Python is fun!"); print("Python is also easy.")
