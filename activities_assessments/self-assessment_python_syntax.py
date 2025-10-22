a40db8a41f75653b29960cd518e98c447b0f421a
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 14:36:57 +0100

    Add self-assessment task from python syntax

---
+++ activities_assessments/self-assessment_python_syntax.py
@@ -0,0 +1,14 @@
+"""
+Name: Nadika Welch
+Date: 22.10.2025
+"""
+
+user_first_name = input("What is your first name? ")
+user_last_name = input("What is your last name? ")
+user_contact = input("What is your phone number? ")
+user_born_year = input("What year were you born? ")
+
+print(f"Your first name is {user_first_name}.")
+print(f"Your last name is {user_last_name}.")
+print(f"Your phone number is {user_contact}")
+print(f"You were born in {user_born_year}")
