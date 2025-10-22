ae25c85af180a617924ff5735225c1acffefd981
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 15:08:08 +0100

    Add activity 2 task from python data types

--- activities_assessments/activities_basic_python_data_types.py
+++ activities_assessments/activities_basic_python_data_types.py
@@ -22,3 +22,25 @@ number = float(user_number)
 # convert to integer (drops decimal part)
 converted_integer = int(number)
 print(f"The value is {converted_integer} with type {type(converted_integer)}.")
+
+# ***
+
+"""
+Activity 2
+Update the code below so that the result is equal to 576.
+Do not change any of the existing values or operators or the order in which they appear.
+"""
+
+# do not change the order in which the numbers and operators appear in the next line
+# result = 5 + 3**2 * 9
+
+# print(result)  # the output should be 576
+
+
+# Solution:
+# I added parentheses to change the order of evaluation
+result = (5 + 3) ** 2 * 9 
+
+print(result) 
+
+# ***
