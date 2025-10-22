e3c4e20bb322044a1534756cf41a6a7170f152c4
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 12:51:51 +0100

    Add Activity 1 in python syntax

---
+++ activities_assessments/activities_python_syntax.py
@@ -0,0 +1,7 @@
+# Activity 1
+# display the text in quotation marks to an output block
+# Print("Python is fun!")
+
+# Fixed code
+# we use lowercase for functions. Python is case-sensitive language. 
+print("Python is fun!")
