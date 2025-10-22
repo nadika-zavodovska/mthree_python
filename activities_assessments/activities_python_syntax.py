339bd97b069531126060aeeb886caabc64d61f95
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-22 13:18:04 +0100

    Add Activity 1 tasks from python syntax

--- activities_assessments/activities_python_syntax.py
+++ activities_assessments/activities_python_syntax.py
@@ -1,18 +1,60 @@
 # Activity 1
+
+# Task #1
 # display the text in quotation marks to an output block
 # Print("Python is fun!")
 
-# Fixed code
-# We use lowercase for functions. Python is case-sensitive language.
+"""" Fixed code
+We use lowercase for functions. Python is case-sensitive language.
+"""
 print("Python is fun!")
 
 # ***
 
-# Activity 2
-# Display the text in quotation marks to an output block 
+# Task #2
+# Display the text in quotation marks to an output block
 # without moving any of the existing acode to a different line
 # print("Python is fun!") print("Python is also easy.")
 
-# Fixed code
-# Add ; to make possible write multiple lines on one line
+""" Fixed code
+I added ; to make possible write multiple lines on one line
+"""
 print("Python is fun!"); print("Python is also easy.")
+
+# ***
+
+# Task #3
+# Display the text in quotation marks to an output block
+# without moving any of the existing code to a different line
+# print
+# ("Python is fun!")
+
+""" Fixed code
+I added \ . It treats next line as it is a part of the same line
+"""
+print \
+("Python is fun!")
+
+# ***
+
+# Task #4
+# Change each variable name to an appropriate name for Python.
+# Do not use the same variable name more than one time.
+# 1-name = "Rebecca" # first name
+# &_name = "Roberts" # last name
+
+# After changing the variable names, update the code below
+# to print out each name.
+# print(1-name)
+# print(&_name)
+
+"""Fixed code:
+There is a rule - we can't start variable names with a number or symbol"""
+first_name = "Rebecca" # first name
+last_name = "Roberts" # last name
+
+print(first_name)
+print(last_name)
+
+
+# Activity 2
