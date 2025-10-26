44206b22396bfed52b62fe60e5b0d68dbaf3d29e
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-10-26 18:17:20 +0000

    Append, insert list methods

--- list_methods/append_insert_methods.py
+++ list_methods/append_insert_methods.py
@@ -1,7 +1,49 @@
-cities = ["London", "Munich"]
-
 # Append "Paris" and "Madrid" one by one
+cities = ["London", "Munich"]
 
 cities.append("Paris")
 cities.append("Madrid")
 print(cities)
+
+# *****
+
+# Insert 30 at index 2
+
+nums = [10, 20, 40]
+
+nums.insert(2, 30)
+
+print(nums)
+
+# ****
+
+# Use insert() not append to add "d" at the end
+letters = ["a", "b", "c"]
+
+letters.insert(len(letters), "d")
+print(letters)
+
+# ****
+# Insert "banana" between apple and orange
+fruits = ["apple", "orange", "mango"]
+
+fruits.insert(1, "banana")
+print(fruits)
+
+# ****
+# Using a loop and only append(), add numbers 1 to 5.
+nums = []
+
+for num in range(1, 6):
+    nums.append(num)
+    
+print(nums)
+
+# ****
+# Using a loop and only insert(0, x), add numbers 1 to 5
+nums = []
+
+for num in range(1,6):
+    nums.insert(0, num)
+    
+print(nums)
