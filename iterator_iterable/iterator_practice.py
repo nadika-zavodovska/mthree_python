1e2910e8cbbb5c5e03cd411d7ad406447e6d626c
Nadika Zavodovska <nadikazavodovska@gmail.com>
2025-11-01 19:21:23 +0000

    Add repeat, takewhile, chain methods

--- iterator_iterable/iterator_practice.py
+++ iterator_iterable/iterator_practice.py
@@ -1,4 +1,4 @@
-from itertools import count, cycle
+from itertools import count, cycle, repeat, chain, islice, takewhile
 
 # Pure iterator usage
 
@@ -58,12 +58,85 @@ counter = count(10)
 
 # print("Done")
 
-# using itertools.cycle(), cycle through
-colors = ["red", "green", "blue"]
-iter_colors = iter(colors)
-cycle_colors = cycle(iter_colors)
-printed_colors = 0
+# Generate numbers starting at 10, step 2, and stop after you print 6 numbers.
 
-while printed_colors < 5:
-    print(next(cycle_colors))    
-    printed_colors += 1
+# counter_num = count(10, 2)
+# printed_num = 0
+
+# while printed_num < 6:
+#     print(next(counter_num))
+#     printed_num += 1
+
+# print("Done")
+
+# Cycle through this list:
+# colors_list = ["red", "green", "blue"]
+# cycle_colors_list = cycle(colors_list)
+# printed = 0
+
+# while printed < 8:
+#     print(next(cycle_colors_list))
+#     printed += 1
+
+# Use repeat("Python", 3) to pair each "Python" with numbers 0,1,2 using zip.
+# repeat("Python", 3)
+
+# with for loop
+# for word, num in zip(repeat("Python", 3), range(3)):
+#     print(f"{word} {num}")
+
+# with while loop
+# pairs = zip(repeat("Python", 3), range(3))
+# pairs_iter = iter(pairs)
+
+# while True:
+#     try:
+#         word, num = next(pairs_iter)
+#         print(f"{word} {num}")
+#     except StopIteration:
+#         break
+
+# Combine lists without using +:
+
+# a = [1, 2, 3]
+# b = [4, 5]
+# c = [6]
+
+# print(list(chain(a, b, c)))
+
+# Print the first 5 numbers from range(100, 200) using islice()
+
+# with fol loop
+# for value in islice(range(100, 200), 5):
+#     print(value)
+
+# # with while loop
+# slice_num = islice(range(100, 200), 5)
+
+# while True:
+#     try:
+#         print(next(slice_num))
+#     except StopIteration:
+#         break
+
+# Using takewhile, print numbers until one is >= 10
+# with for loop
+# nums = [1, 2, 3, 10, 11, 12]
+
+# for num in takewhile(lambda x: x < 10, nums):
+#     print(num)
+
+# with while loop
+nums = [1, 2, 3, 10, 11, 12]
+it = iter(nums)
+
+while True:
+    try:
+        value = next(it)
+        if value < 10:
+            print(value)
+        else:
+            break        
+    except StopIteration:
+        break
+        
